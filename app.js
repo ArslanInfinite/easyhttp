bd9d71f37f3e2b3fd80e7022b8ec26ba1daca4df
Arslan <ali.arslan319@gmail.com>
2021-10-06 03:07:47 -0400

    create function to get singular post in app.js

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const http = new easyHTTP
 
 // GET Posts
 
-http.get('https://jsonplaceholder.typicode.com/posts1', 
-function(error, response){
-    if(error) {
-        console.log(error)
-    } else {
-        console.log(response)
-    }
-    } 
-)
+// http.get('https://jsonplaceholder.typicode.com/posts1', 
+// function(error, response){
+//     if(error) {
+//         console.log(error)
+//     } else {
+//         console.log(response)
+//     }
+//     } 
+// )
+
+// // get single post
+// // http.get('https://jsonplaceholder.typicode.com/posts/1', 
+// // function(error, post){
+// //     if(error) {
+// //         console.log(post)
+// //     } else {
+// //         console.log(post)
+
+// //     }
+// // } )
+
