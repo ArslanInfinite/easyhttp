af1931538ce175fe1190a93d67dc8fa51d201ab6
Arslan <ali.arslan319@gmail.com>
2021-10-05 23:38:55 -0400

    create GET request using callback

---
+++ easyhttp.js
@@ -0,0 +1,22 @@
+function easyHTTP(){
+    this.http = new XMLHttpRequest()
+}
+
+// making http GET request 
+easyHTTP.prototype.get = function(url, callback){
+    this.http.open('GET', url, true)
+    // self will capture the scope of this 
+    let self = this 
+    this.http.onload = function(){
+        if(this.http.status === 200) {
+            callback(self.http.responseText)
+        }        
+    }
+    this.http.send
+}
+
+// making http POST request 
+
+// making http PUT request 
+
+// making http DELETE request 
